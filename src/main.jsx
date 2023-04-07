e98ac9ed7cd55ca96dfdf9617e627aaf72327d6b
ovi-khan <manikkhan2240@gmail.com>
2023-04-07 16:36:39 +0600

    react router setup for tshirt project

---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import React, { Children } from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './Components/Layout/Main';
+import OrderReview from './Components/OrderReview/OrderReview';
+import Home from './Components/Home/Home';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        loader: () => fetch('tshirts.json')
+      },
+      {
+        path: 'review',
+        element: <OrderReview></OrderReview>
+      }
+    ]
+  }
+  ])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
